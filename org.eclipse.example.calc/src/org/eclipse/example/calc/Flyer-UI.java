959faf19afc4df15aeefc86f8abcfe5c67ff7437
xcb7216 <Wolfgang.Graf@fiduciagad.de>
2021-02-23 12:26:37 +0100

    Bitte neues File mit in den master übernehmen, Vorbereitung für die Flyer Aktion

---
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/Flyer-UI.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (C) 2021, Wolfgang Graf @fiduciagad.dr
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.example.calc;
+
+public interface Flyer-UI {
+	/**
+	 * @return name of the operation
+	 * hier muessen nich die passenden Routinen eingesetzt werden
+	 */
+	public String getName();
+}
